b1936ffcb1292d53d1a453d734ee32b6f24fb33a
malchak <michaelmalchak@gmail.com>
2013-11-17 21:17:05 -0800

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Gaproto2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaproto2::Application.config.secret_key_base = '5d9c38bcbd69e0934439399a5f9af902fff55d890181a547f0b0ae6d945cefe187031c36e188a60c51a6f5e7cb6992d9b177cd4c3c2f5fd9c8f6b5e20c93c787'
